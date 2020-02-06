b1b13f632426957ba29f029abe0160b9cd6d0e8a
Wawan <rachmad.kurniawan@ralali.com>
2020-02-07 01:41:35 +0700

    feat: add routes

---
+++ routes/article.go
@@ -0,0 +1,50 @@
+package routes
+
+import (
+	"github.com/wawandx/rest-api-gin/config"
+	"github.com/wawandx/rest-api-gin/models"
+	"github.com/gin-gonic/gin"
+	"github.com/gosimple/slug"
+)
+
+func GetHome(context *gin.Context) {
+	items := []models.Article{}
+	config.DB.Find(&items)
+
+	context.JSON(200, gin.H {
+		"status": "success",
+		"data": items,
+	})
+}
+
+func GetArticle(context *gin.Context) {
+	slug := context.Param("slug")
+
+	var item models.Article
+
+	if config.DB.First(&item, "slug = ?", slug).RecordNotFound() {
+		context.JSON(404, gin.H{"status": "error", "message": "record not found"})
+		context.Abort() //membatalkan semua fungsi yang akan d jalankan di bawah
+		return
+	}
+
+	context.JSON(200, gin.H {
+		"status": "success",
+		"data": item,
+	})
+}
+
+func PostArticle(context *gin.Context) {
+	item := models.Article {
+		Title : context.PostForm("title"),
+		Desc : context.PostForm("desc"),
+		Slug : slug.Make(context.PostForm("title")),
+	}
+
+	config.DB.Create(&item)
+
+	context.JSON(200, gin.H {
+		"status": "success",
+		"data": item,
+	})
+}
