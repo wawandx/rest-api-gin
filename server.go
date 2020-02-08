969f81b22354df5255167a7c785e3ef79c22e3ba
Wawan <rachmad.kurniawan@ralali.com>
2020-02-08 20:06:51 +0700

    chore: add routing auth in server.go

--- server.go
+++ server.go
@@ -4,16 +4,21 @@ import (
 	"github.com/wawandx/rest-api-gin/config"
 	"github.com/wawandx/rest-api-gin/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/subosito/gotenv"
 )
 
 func main() {
 	config.InitDB()
 	defer config.DB.Close()
+	gotenv.Load()
 
 	router := gin.Default()
 
 	v1 := router.Group("/api/v1/")
 	{
+		v1.GET("/auth/:provider", routes.RedirectHandler)
+		v1.GET("/auth/:provider/callback", routes.CallbackHandler)
+
 		articles := v1.Group("/article")
 		{
 			articles.GET("/", routes.GetHome)
