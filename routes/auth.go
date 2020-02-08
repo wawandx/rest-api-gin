2efb2992fa704a88aa74916c3fb0ccd4e0477650
Wawan <rachmad.kurniawan@ralali.com>
2020-02-08 20:05:42 +0700

    chore: add routes and function for auth

---
+++ routes/auth.go
@@ -0,0 +1,76 @@
+package routes
+
+import (
+	"os"
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/wawandx/rest-api-gin/config"
+)
+
+// Redirect to correct oAuth URL
+func RedirectHandler(c *gin.Context) {
+	// Retrieve provider from route
+	provider := c.Param("provider")
+
+	providerSecrets := map[string]map[string]string{
+		"github": {
+			"clientID":     os.Getenv("CLIENT_ID_GITHUB"),
+			"clientSecret": os.Getenv("CLIENT_SECRET_GITHUB"),
+			"redirectURL":  os.Getenv("AUTH_REDIRECT_URL") + "/github/callback",
+		},
+		"google": {
+			"clientID":     os.Getenv("CLIENT_ID_GOOGLE"),
+			"clientSecret": os.Getenv("CLIENT_SECRET_GOOGLE"),
+			"redirectURL":  os.Getenv("AUTH_REDIRECT_URL") + "/google/callback",
+		},
+	}
+
+	providerScopes := map[string][]string{
+		"github":   []string{},
+		"google": []string{},
+	}
+
+	providerData := providerSecrets[provider]
+	actualScopes := providerScopes[provider]
+	authURL, err := config.Gocial.New().
+		Driver(provider).
+		Scopes(actualScopes).
+		Redirect(
+			providerData["clientID"],
+			providerData["clientSecret"],
+			providerData["redirectURL"],
+		)
+
+	// Check for errors (usually driver not valid)
+	if err != nil {
+		c.Writer.Write([]byte("Error: " + err.Error()))
+		return
+	}
+
+	// Redirect with authURL
+	c.Redirect(http.StatusFound, authURL)
+}
+
+// Handle callback of provider
+func CallbackHandler(c *gin.Context) {
+	// Retrieve query params for state and code
+	state := c.Query("state")
+	code := c.Query("code")
+	provider := c.Param("provider")
+
+	// Handle callback and check for errors
+	user, token, err := config.Gocial.Handle(state, code)
+	if err != nil {
+		c.Writer.Write([]byte("Error: " + err.Error()))
+		return
+	}
+
+	// Print in terminal user information
+	fmt.Printf("%#v", token)
+	fmt.Printf("%#v", user)
+	fmt.Printf("%#v", provider)
+
+	// If no errors, show provider name
+	c.Writer.Write([]byte("Hi, " + user.FullName))
+}
